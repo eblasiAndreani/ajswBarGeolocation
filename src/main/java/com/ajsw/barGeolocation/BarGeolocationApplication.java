e2d8857b63c29af511ee4320310d4da8735c1cc3
Blasi Eduardo <eblasi@andreani.com>
2023-09-17 00:08:01 -0300

    Commit initial

---
+++ src/main/java/com/ajsw/barGeolocation/BarGeolocationApplication.java
@@ -0,0 +1,13 @@
+package com.ajsw.barGeolocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarGeolocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarGeolocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajsw/barGeolocation/BarGeolocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajsw.barGeolocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarGeolocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
